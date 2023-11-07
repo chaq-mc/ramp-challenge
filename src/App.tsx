c39d6706177554733552561607c0ca63a5215e72
Chequel McNeil <mcneil_chequel@bah.com>
2023-11-07 18:39:22 -0500

    Implement fetch to api and loading animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import Word from './Word';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const url = 'https://wgg522pwivhvi5gqsn675gth3q0otdja.lambda-url.us-east-1.on.aws/686572';
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [word, setWord] = useState("");
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.text()
+      .then((text) => {
+        setWord(text);
+        setIsLoading(false);
+      }))
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {isLoading ? <p>Loading...</p> : <Word word={word}/>}
     </>
   )
 }
